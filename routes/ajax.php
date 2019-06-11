cef32a8c95b1d4e976ca8270c895770fcba11579
adamthehutt <adam.huttler@fracturedatlas.org>
2019-06-11 13:38:42 -0400

    fix (?) route definition

--- routes/ajax.php
+++ routes/ajax.php
@@ -4,6 +4,4 @@ declare(strict_types=1);
 use AdamTheHutt\LaravelDismissibleTips\DismissibleTipsController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('dismissible-tips')->middleware('auth')->group(function () {
-    Route::post('dismiss', [DismissibleTipsController::class, 'dismiss']);
-});
+Route::post('dismissible-tips/dismiss', [DismissibleTipsController::class, 'dismiss'])->middleware(['auth']);

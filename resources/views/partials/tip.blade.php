e7a85e0eb750cdfdbabbc6db6be4727fbfc19cf3
Adam Huttler <adam.huttler@gmail.com>
2019-08-16 07:04:30 -1000

    Allow translation files outside vendor namespace

--- resources/views/partials/tip.blade.php
+++ resources/views/partials/tip.blade.php
@@ -6,7 +6,14 @@
             <i class="fas fa-info-circle fa-2x"></i>
         </div>
         <div class="col-xs-10">
-            {{ __("dismissible-tips::tips.$tip") }} @isset($link)&nbsp;&nbsp;<a href="{!! $link !!}"><i class="fas fa-arrow-circle-right"></i></a> @endisset
+            @if (\Illuminate\Support\Facades\Lang::has($tip))
+                @lang($tip)
+            @else
+                @lang("dismissible-tips::tips.$tip")
+            @endif
+            @isset($link)&nbsp;&nbsp;
+                <a href="{!! $link !!}"><i class="fas fa-arrow-circle-right"></i></a>
+            @endisset
         </div>
     </div>
 </div>
